81ff12abc21a95d657c274c3533607f6d95577b9
[01;31m[K"name"[m[K: "Jonathan Huang <[01;31m[K"email"[m[K: null>
2016-01-24 15:04:12 -0800

    Validate user email and encrypts user password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,26 @@
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+  validates :email, presence: true
+
+  validate :unique_email, :password
+
+  def password
+    if hashed_password
+      @password ||= BCrypt::Password.new(hashed_password)
+    else
+      errors.add(:password, "can't be left blank")
+    end
+  end
+
+  def password=(new_password)
+    unless new_password.empty?
+      @password = BCrypt::Password.create(new_password)
+      self.hashed_password = @password
+    end
+  end
+
+  def unique_email
+    if User.find_by(email: email)
+      errors.add(:email, "has already been taken")
+    end
+  end
 end
